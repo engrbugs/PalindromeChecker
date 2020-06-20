32a3be9726a6af55a2f3e9a240eb726e4c14053b
engrbugs <engrbugs@gmail.com>
2020-06-20 15:44:53 -0700

    done

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('hw');
+function palindrome(str) {
+    let cleanStr = str.replace(/[_\W]/g, '').toLowerCase();
+    return cleanStr === cleanStr.split('').reverse().join('');
+  }
+
+  
+  console.log(palindrome("My age is 0, 0 si ega ym."));
+  
